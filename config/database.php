cca9443eba1b0e00fd5e701839b4f7c1fb8cc4ed
WillianChavez <williamchavezamaya@gmail.com>
2024-06-11 11:01:56 -0600

    init

---
+++ config/database.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'driver'    => 'mysql',
+    'host'      => '127.0.0.1',
+    'database'  => 'your_database',
+    'username'  => 'your_username',
+    'password'  => 'your_password',
+    'charset'   => 'utf8',
+    'collation' => 'utf8_unicode_ci',
+    'prefix'    => '',
+];
